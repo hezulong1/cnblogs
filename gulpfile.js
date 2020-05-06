af9b78a8ea1880fb9d811e0d25888e9d7cb13443
mashzl <626993211@qq.com>
2020-05-06 19:21:01 +0800

    new commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,21 @@ const { src, dest, watch } = require('gulp');
 const cleanCss = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
-const gulpif = require('gulp-if');
 const rename = require('gulp-rename');
-const connect = require('gulp-connect');
 
-const isProd = process.env.NODE_ENV === 'production';
-console.log('环境：', process.env.NODE_ENV);
 
 function build(cb) {
   cb();
 
-  const hash = Math.random().toString(36).slice(4);
-  const fileName = isProd ? `${hash}.min.css` : 'custom.css';
-  console.log('文件名：', fileName);
-
   return src('src/index.scss')
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // 嵌套输出方式 nested, 展开输出方式 expanded, 紧凑输出方式 compact, 压缩输出方式 compressed
+    .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(gulpif(isProd, cleanCss()))
-    .pipe(rename(fileName))
-    .pipe(dest(isProd ? 'dist' : 'skin'))
-    .pipe(gulpif(!isProd, connect.reload()));
+    .pipe(cleanCss())
+    .pipe(rename('style.css'))
+    .pipe(dest('dist'));
 }
 
 exports.build = build;
-
-exports.connect = function() {
-  connect.server({
-    root: 'skin',
-    livereload: true
-  })
-}
 
 exports.default = function() {
   watch('src/**/*', { delay: 500 }, build);
